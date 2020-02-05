88afb3ee92e6592b21b3c3ddfcf2d3f284b296e6
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2020-02-05 02:02:17 +0000

    updated args.c

--- args.c
+++ args.c
@@ -5,7 +5,8 @@
 
 int main(int argc, char **argv){
     for(int i = 0; i < argc; ++i){
-    printf("%s\n", argv[i]);
+        printf("%s\n", *argv);
+        argv++;
     }    
     return EXIT_SUCCESS; 
 }
